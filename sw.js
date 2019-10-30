f0bd798350b8970a3ae3a7cfcb253da508fee6da
Byeongjin Kang <jasonkang14@gmail.com>
2019-10-30 23:01:11 +0900

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-c46ed7afd660b9760d37.js"
+    "url": "webpack-runtime-c5efb885ee1c62f9c2f2.js"
   },
   {
     "url": "styles.c6410b3e7a33fce9850f.css"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "4fb36a91f484cc64adb8e72e9950b93d"
+    "revision": "9c04b4d8928869d0c3e19d09792031ba"
   },
   {
     "url": "manifest.json",
