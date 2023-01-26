3fbc1f630d9fee3f18e8b4305519fe13a65747e1
Jason Kang <jasonkang14@gmail.com>
2023-01-26 21:49:06 +0900

    Fix adsense settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-google-adsense',
       options: {
-        publisherId: 'pub-2002611361597206'
+        publisherId: 'pub-8427179398064162'
       },
     },
     {
